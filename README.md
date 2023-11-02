7582b1ef14cb153562a3b662cd82f236b93bff83
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-11-02 12:18:55 +0100

    make possible to add a new todo

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-node server.js
+# htmx-ejs
+
+Trying out [HTMX](https://htmx.org/) with EJS server side templates.
+
+## Usage
+
+`npm run dev`
+
+## Sources
 
 source: https://www.digitalocean.com/community/tutorials/how-to-use-ejs-to-template-your-node-application
--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function(req, res) {
-  res.render('pages/index');
+app.get("/", function (req, res) {
+  res.render("pages/index");
 });
 
 // about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.get("/about", function (req, res) {
+  res.render("pages/about");
+});
+
+app.post("/foo", function (req, res) {
+  res.send({ baz: "bat" });
+});
+
+app.post("/new-todo", function (req, res) {
+  console.log(req.body);
+  res.render("partials/todos", { data: req.body });
 });
 
 app.listen(8080);
-console.log('Server is listening on port 8080');
+console.log("Server is listening on port 8080");
