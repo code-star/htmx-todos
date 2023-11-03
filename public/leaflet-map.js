f08b778c991594796d83ba436b8114632c0aa878
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-11-03 14:04:35 +0100

    add leaflet

---
+++ public/leaflet-map.js
@@ -0,0 +1,38 @@
+class LeafletMap extends HTMLElement {
+  static observedAttributes = ["x", "y"];
+
+  constructor() {
+    super();
+    this.x = 51;
+    this.y = 1;
+    this.mapElem = null;
+  }
+
+  connectedCallback() {
+    this.innerHTML = `<div>
+        <div id="map"></div>
+    </div>`;
+
+    this.mapElem = L.map("map").setView([this.x, this.y], 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.mapElem);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    setTimeout(() => {
+      console.log(
+        `Attribute ${name} has changed from ${oldValue} to ${newValue}.`
+      );
+      if (name === "x") {
+        this.mapElem.setView([newValue, this.y], 13);
+      }
+      if (name === "y") {
+        this.mapElem.setView([this.x, newValue], 13);
+      }
+    }, 1000);
+  }
+}
